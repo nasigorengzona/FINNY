aeee0f0b721e8fdf870f48087065fa6cd3861617
ererer <ee80040@student.uph.edu>
2023-01-08 20:48:25 +0700

    Initial Commit

---
+++ FINNY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FINNY
+//
+//  Created by erlina ng on 08/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
